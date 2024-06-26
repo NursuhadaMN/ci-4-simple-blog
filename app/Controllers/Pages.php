35ee992037a13c2243c5171bd1998ff8b9ecc9d2
NursuhadaMN <nursuhadamn98@gmail.com>
2024-06-26 16:13:29 +0800

    Initial Commit

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,14 @@
+<?php namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        return 'This is the Pages Controller -> Index Function';
+    }
+
+    public function showme($page = 'home')
+    {
+        return 'This Page is '.$page;
+    }
+}
